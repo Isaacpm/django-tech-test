649e8a0c82f59f87992c36d00db55bccf5ffefee
Isaac Perez Moncho <isaac.perez.moncho@gmail.com>
2016-11-20 06:06:53 +0000

    Added models to the admin file to make them available in the admin interface

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from loans.models import Loan, Business, Profile
+
+# Register your models here.
+admin.site.register(Business)
+admin.site.register(Loan)
+admin.site.register(Profile)
